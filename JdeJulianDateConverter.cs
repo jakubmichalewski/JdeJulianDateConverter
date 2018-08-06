e4b220b9d65e5e3487800ac1b1c7879ec5cbb829
MichaJ01 <jakub.michalewski@gs-fresh.com>
2018-08-06 12:50:13 +0100

    calculation for century added

--- JdeJulianDateConverter.cs
+++ JdeJulianDateConverter.cs
@@ -7,8 +7,10 @@ namespace JdeJulianDateConverter
         public static DateTime JdeJulianToDateTime(this int num)
         {
             var stringifiedJulianDate = num.ToString();
-            if (stringifiedJulianDate.Length != 6) throw new Exception("Can't convert Julian Date to Date time, invalid format");
-            var baseYear = (stringifiedJulianDate.Substring(0, 1) == "0") ? 1900 : 2000;
+            if (stringifiedJulianDate.Length != 6) 
+                throw new Exception("Can't convert Julian Date to Date time, invalid format");
+
+            var baseYear = 1900 + Convert.ToInt32(stringifiedJulianDate.Substring(0, 1)) * 100;
             var year = baseYear + Convert.ToInt32(stringifiedJulianDate.Substring(1, 2));
             var days = Convert.ToInt32(stringifiedJulianDate.Substring(stringifiedJulianDate.Length - 3));
             return new DateTime(year, 1, 1).AddDays(days - 1);
