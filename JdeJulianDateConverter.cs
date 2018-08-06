bc7b18ab0355ea6d33a5b9a31e82f4c7880cc6ff
MichaJ01 <jakub.michalewski@gs-fresh.com>
2018-08-06 12:22:38 +0100

    simplified toString operation

--- JdeJulianDateConverter.cs
+++ JdeJulianDateConverter.cs
@@ -6,9 +6,10 @@ namespace JdeJulianDateConverter
     {
         public static DateTime JdeJulianToDateTime(this int num)
         {
-            var baseYear = (num.ToString().Substring(0, 1) == "0") ? 1900 : 2000;
-            var year = baseYear + Convert.ToInt32(num.ToString().Substring(1, 2));
-            var days = Convert.ToInt32(num.ToString().Substring(num.ToString().Length - 3));
+            var stringifiedJulianDate = num.ToString();
+            var baseYear = (stringifiedJulianDate.Substring(0, 1) == "0") ? 1900 : 2000;
+            var year = baseYear + Convert.ToInt32(stringifiedJulianDate.Substring(1, 2));
+            var days = Convert.ToInt32(stringifiedJulianDate.Substring(stringifiedJulianDate.Length - 3));
             return new DateTime(year, 1, 1).AddDays(days - 1);
         }
     }
