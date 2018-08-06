cf4760851762d8affbe1145b1b3476c144268403
MichaJ01 <jakub.michalewski@gs-fresh.com>
2018-08-06 12:45:30 +0100

    Exception on invalid format

--- JdeJulianDateConverter.cs
+++ JdeJulianDateConverter.cs
@@ -7,6 +7,7 @@ namespace JdeJulianDateConverter
         public static DateTime JdeJulianToDateTime(this int num)
         {
             var stringifiedJulianDate = num.ToString();
+            if (stringifiedJulianDate.Length != 6) throw new Exception("Can't convert Julian Date to Date time, invalid format");
             var baseYear = (stringifiedJulianDate.Substring(0, 1) == "0") ? 1900 : 2000;
             var year = baseYear + Convert.ToInt32(stringifiedJulianDate.Substring(1, 2));
             var days = Convert.ToInt32(stringifiedJulianDate.Substring(stringifiedJulianDate.Length - 3));
